8c7cebf4803fbe9d6357eab2651a1731fe4aa729
Ankit Gupta <ankit.gupta8898@gmail.com>
2014-02-23 18:20:09 -0500

    Demo App Scroll reveal rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScrollReveal::Application.config.secret_key_base = '24b77cb84b3bbfaf6dde5f2c756af87bda4170156c0d909cac56eebd7df53072fecbc7a6549443698918deec07476db7ba0870d65aed92a997e2c6419649cabf'
